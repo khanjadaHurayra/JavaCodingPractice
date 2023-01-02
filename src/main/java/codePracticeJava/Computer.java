94d87d773e457857d55686e3f7c2a65bc0128115
Khanjada Hurayra <khanjada@hotmail.com>
2023-01-02 12:01:51 -0500

    JavaCodePractice2

--- src/main/java/codePracticeJava/Computer.java
+++ src/main/java/codePracticeJava/Computer.java
@@ -11,6 +11,7 @@ public class Computer {
 	}
 
 	public static void main(String[] args) {
+		
 		Computer my = new Computer();
 		Laptop your = new Laptop();
 
---
+++ src/main/java/codePracticeJava/TestThrow.java
@@ -0,0 +1,20 @@
+package codePracticeJava;
+
+public class TestThrow {
+
+	static void validate(int age){
+
+		if(age<18)
+			throw new ArithmeticException("not valid");
+
+		else
+			System.out.println("welcome to vote on Technolamror");
+	}
+
+	public static void main(String args[]){
+
+		validate(13);
+		System.out.println("rest of the code...");
+	}
+
+}
